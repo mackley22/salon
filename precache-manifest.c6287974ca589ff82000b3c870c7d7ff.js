f6b963810870cb5cdc1d76da31172ef908c58de2
Michelle Ackley <ackleym22@gmail.com>
2019-07-22 23:12:36 -0400

    Updates

---
+++ precache-manifest.c6287974ca589ff82000b3c870c7d7ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c519e4cc47b3933466338c77d9b35ee",
+    "url": "/salon/index.html"
+  },
+  {
+    "revision": "a4ef426af0149030753e",
+    "url": "/salon/static/css/2.606264ab.chunk.css"
+  },
+  {
+    "revision": "8336cde487ab9373b13f",
+    "url": "/salon/static/css/main.80ceaef8.chunk.css"
+  },
+  {
+    "revision": "a4ef426af0149030753e",
+    "url": "/salon/static/js/2.f4044c6b.chunk.js"
+  },
+  {
+    "revision": "8336cde487ab9373b13f",
+    "url": "/salon/static/js/main.b8ae21b0.chunk.js"
+  },
+  {
+    "revision": "3f49d559002fdd8a6ca4",
+    "url": "/salon/static/js/runtime~main.4f044513.js"
+  },
+  {
+    "revision": "bef1f3246e45e91ef9ac99055a607b43",
+    "url": "/salon/static/media/logo8.bef1f324.jpg"
+  }
+]);
